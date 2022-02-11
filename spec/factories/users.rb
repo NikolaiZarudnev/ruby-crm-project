0bd7b17dbd3495fcc0abf793b4c434ac51d10970
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2022-02-11 21:51:55 +0700

    users

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    name { "MyString" }
+    password_digest { "MyString" }
+    phone_number { "MyString" }
+    role { "MyString" }
+  end
+end
