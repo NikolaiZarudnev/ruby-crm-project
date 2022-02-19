0129b8527ac1c2e36cfb8caa65273096f913a4ee
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2022-02-19 09:37:42 +0700

    start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  #private
-  include ApplicationHelper
-  
-  #helper_method :current_user, :user_signed_in?, :log_in
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
 module ApplicationHelper
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
-      end
-    
-      def user_signed_in?
-        current_user.present?
-      end
-    
-      def log_in(user)
-        session[:user_id] = user.id
-      end
-    
-      def log_out
-        session.delete :user_id
-      end
 end
