baa472bf22d728874a8275e9389c3e308b17c1ac
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2022-02-26 22:08:52 +0700

    fix

--- db/migrate/20220209225740_create_users.rb
+++ db/migrate/20220209225740_create_users.rb
@@ -3,10 +3,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.string :phone_number
-      t.boolean :is_admin, null: false
-      t.boolean :is_supplier, null: false
-      t.boolean :is_operator, null: false
-      t.boolean :is_customer, null: false
+      t.boolean :is_admin
+      t.boolean :is_supplier
+      t.boolean :is_operator
+      t.boolean :is_customer
       t.timestamps
     end
   end
